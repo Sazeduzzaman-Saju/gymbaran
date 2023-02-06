c3708d246055e2534f9d9212a8672c7cdc96dc10
Sazeduzzaman <sazeduzzaman@yahoo.com>
2023-02-06 15:56:38 +0600

    Structure Ready

---
+++ src/pages/Header/Header.js
@@ -0,0 +1,99 @@
+import Image from 'next/image';
+import Link from 'next/link';
+import React, { useState } from 'react';
+import logo from '../../img/logo.png'
+
+const Header = () => {
+    const [active, setActive] = useState(false);
+
+    const showMenu = () => {
+        setActive(!active);
+    };
+    const MenuItems = ({ showMenu, active }) => {
+        return (
+            <ul
+                className={
+                    active
+                        ? "flex-col flex items-center fixed inset-0 left-1/4 uppercase bg-black/40 backdrop-blur-lg gap-8 justify-center p-8 md:hidden"
+                        : "hidden"
+                }
+            >
+                <button onClick={showMenu} className="btn btn-primary">close menu</button>
+
+                <li>
+                    <Link href="/">
+                        Home
+                    </Link>
+                </li>
+                <li>
+                    <Link href="/projects">
+                        Program
+                    </Link>
+                </li>
+
+                <li>
+                    <Link href="/about">
+                        Blog
+                    </Link>
+                </li>
+                <li>
+                    <Link href="/contact">
+                        About Us
+                    </Link>
+                </li>
+            </ul>
+        );
+    };
+
+    return (
+        <div>
+            <nav className='container mx-auto '>
+
+                <div className='flex justify-between items-center py-1'>
+                    <div className='flex justify-center items-center'>
+                        <h1 className='text-4xl'>
+                            <Image src={logo} alt="/"></Image>
+                        </h1>
+                    </div>
+
+                    <div className="absolute right-0 md:hidden ">
+                        <button onClick={showMenu} className="btn btn-primary">
+                            Show Menu
+                        </button>
+                    </div>
+
+                    <div>
+                        <ul className="hidden  md:flex justify-between items-center gap-8 p-6  bg-white/10">
+                            <li>
+                                <Link href="/">
+                                    Home
+                                </Link>
+                            </li>
+                            <li>
+                                <Link href="/">
+                                    Program
+                                </Link>
+                            </li>
+                            <li>
+                                <Link href="/">
+                                    Blog
+                                </Link>
+                            </li>
+
+                            <li>
+                                <Link href="/about">
+                                    About Us
+                                </Link>
+                            </li>
+                            <button className='btn btn-primary bg-[#264373]'>Get Started</button>
+                        </ul>
+                    </div>
+                </div>
+                <MenuItems showMenu={showMenu} active={active} />
+            </nav>
+        </div>
+    );
+};
+
+
+export default Header;
--- src/pages/_app.js
+++ src/pages/_app.js
@@ -1,5 +1,13 @@
 import '@/styles/globals.css'
+import Footer from './Footer/Footer'
+import Header from './Header/Header'
 
 export default function App({ Component, pageProps }) {
-  return <Component {...pageProps} />
+  return (
+    <>
+      <Header></Header>
+      <Component {...pageProps} />
+      <Footer></Footer>
+    </>
+  )
 }
